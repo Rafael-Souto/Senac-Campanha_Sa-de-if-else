d75181447d1419d4b295cd95e8a4f06c4bff1cb8
Rafaero13 <86630359+Rafael-Souto@users.noreply.github.com>
2022-05-28 19:00:36 +0000

    Foi feito a indentação

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Algoritimo de campanhas de saúde, casais e doação de sangue.
+// Algoritimo de campanhas de saúde, casais e doação de sangue
 
 var sexo, idade, estado_civil,
   sexo = prompt ("Informe o seu sexo");
@@ -10,10 +10,10 @@ if (sexo == 'f'){
   if (idade >= 16) {
     alert ("Venha participar da nossa campanha de saúde!");
   } else if (sexo == 'm'){
-    estado_civil = prompt ("Informe o seu estado cívil");
-      if (estado_civil == "casado"){
-        alert ("Venha participar da nossa campanha para casais.");
-      } else {
-        alert ("Venha participar da nossa campanha de doação de sangue.");
+      estado_civil = prompt ("Informe o seu estado cívil");
+    if (estado_civil == "casado"){
+      alert ("Venha participar da nossa campanha para casais.");
+    } else {
+      alert ("Venha participar da nossa campanha de doação de sangue.");
     }  
 }
